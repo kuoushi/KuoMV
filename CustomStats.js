db250bf756466aa9c7e6f41a6b3a2b4946880e3c
Kuoushi <kuoushi@kuoushi.com>
2015-11-03 07:38:27 -0500

    Stats now save and load properly.

--- CustomStats.js
+++ CustomStats.js
@@ -1,7 +1,7 @@
 //=============================================================================
 // Custom Stats
 // CustomStats.js
-// Version: 0.10
+// Version: 0.10.1
 // Author: Kuoushi
 //=============================================================================
 
@@ -10,7 +10,7 @@ Compat.BE = Compat.BE || {};
 
 //=============================================================================
  /*:
- * @plugindesc v0.10 Allows for custom hidden stats that can be used in damage
+ * @plugindesc v0.10.1 Allows for custom hidden stats that can be used in damage
  * formulas.
  * @author Kuoushi
  *
@@ -28,14 +28,14 @@ Compat.BE = Compat.BE || {};
  * Introduction
  * ============================================================================
  *
- * This plugin allows for the easy addition of custom stats to be used in 
+ * This plugin allows for the easy addition of custom stats to be used in
  * damage formulas and various other places. Simply add them to the plugin
  * parameter in the form of a comma-separated list and they'll be available
  * for use. If you want to give them a custom value then you'll need to create
  * a notetag on each specific actor in the form of:
  *
  * <stat: value>
- * 
+ *
  * In the case of the default/example stats that I've included (feel free to
  * remove those, by the way), you could set the custom value for str on a
  * specific actor to 5 by going to that actor's notes and adding <str: 5>
@@ -49,11 +49,12 @@ Compat.BE = Compat.BE || {};
  * Changelog
  * ============================================================================
  *
- * 0.10 Plugin is now compatible with my StatProg plugin. You may use any stats
- *      created in this plugin may be given a growth rate, min and max in
- *      StatProg. Also, the notetags have moved from the class notetags back to
- *      actors for consistency.
- * 0.05 Plugin with basic functionality created.
+ * 0.10.1 Small bugfix to fix stats saving and loading properly.
+ * 0.10   Plugin is now compatible with my StatProg plugin. You may use any stats
+ *        created in this plugin may be given a growth rate, min and max in
+ *        StatProg. Also, the notetags have moved from the class notetags back to
+ *        actors for consistency.
+ * 0.05   Plugin with basic functionality created.
  *
  */
 //=============================================================================
@@ -88,10 +89,33 @@ Compat.BE = Compat.BE || {};
             }
 
             for(var a in data.meta) {
-                var val = parseInt(data.meta[a]);
-                Compat.cparam[data.id][a] = val;
+                if(Compat.Param.cStats.indexOf(a.toLowerCase()) > -1) {
+                  var val = parseInt(data.meta[a]);
+                  Compat.cparam[data.id][a] = val;
+                }
             }
         }
+    };
+
+    Compat.BE.DataManager_makeSaveContents = DataManager.makeSaveContents;
+    DataManager.makeSaveContents = function() {
+        $gameSystem.storeCustomStats(Compat.Param.cStats);
+        return Compat.BE.DataManager_makeSaveContents.call(this);
+    };
+
+    Compat.BE.DataManager_extractSaveContents = DataManager.extractSaveContents;
+    DataManager.extractSaveContents = function(contents) {
+        Compat.BE.DataManager_extractSaveContents.call(this, contents);
+        Compat.Param.cStats = $gameSystem.getCustomStats();
+    };
+
+
+    Game_System.prototype.storeCustomStats = function(val) {
+        this.customStats    = val;
+    };
+
+    Game_System.prototype.getCustomStats = function() {
+        return this.customStats;
     };
 
 //=============================================================================
